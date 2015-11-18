4a471a14a867a90edd8a69931a48ba66149b5cdf
filigithub <filippo.darrigo@hotmail.com>
2015-11-18 16:25:55 +0100

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(".animsition").animsition({
+  inClass:   'fade-in',
+  outClass:  'fade-out',
+  inDuration:50,
+  outDuration: 5
+}) ;
+
+$('.slides').slick({
+  fade:true,
+  autoplay:true,
+  autoplaySpeed:4000,
+  arrows:false,
+  dots:true
+  });
+    
+  
